a51e8b927a8781b673ba8cc4d324d8e598d0dbbb
Eero af Heurlin <eero.afheurlin@iki.fi>
2020-04-16 16:08:43 +0300

    Add manifests for the debug target

--- create_manifests.py
+++ create_manifests.py
@@ -6,6 +6,7 @@ import subprocess
 
 ARCHS = ["x86_64", "aarch64"]
 TAGS = ["latest", "2.34.0", "2.34.0-alpine", "2.34.0-alpine-3.11"]
+BUILDS = ["librealsense", "librealsense-dbg"]
 
 
 if __name__ == "__main__":
@@ -16,18 +17,21 @@ if __name__ == "__main__":
     tag_commands = []
     inspect_commands = []
     push_commands = []
-    for tag in TAGS:
-        manifestag = "{repo}/librealsense:{tag}".format(repo=reponame, tag=tag)
-        cmd = ["docker", "manifest", "create", manifestag]
-        for arch in ARCHS:
-            cmd.append(
-                "{repo}/librealsense:{arch}-{tag}".format(
-                    repo=reponame, arch=arch, tag=tag
-                )
+    for build in BUILDS:
+        for tag in TAGS:
+            manifestag = "{repo}/{build}:{tag}".format(
+                repo=reponame, tag=tag, build=build
             )
-        tag_commands.append(cmd)
-        inspect_commands.append("docker manifest inspect {}".format(manifestag))
-        push_commands.append(["docker", "manifest", "push", manifestag])
+            cmd = ["docker", "manifest", "create", "--amend", manifestag]
+            for arch in ARCHS:
+                cmd.append(
+                    "{repo}/{build}:{arch}-{tag}".format(
+                        repo=reponame, arch=arch, tag=tag, build=build
+                    )
+                )
+            tag_commands.append(cmd)
+            inspect_commands.append("docker manifest inspect {}".format(manifestag))
+            push_commands.append(["docker", "manifest", "push", manifestag])
 
     if os.environ.get("AUTORUN"):
         for cmd in tag_commands:
